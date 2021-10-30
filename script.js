10e8fc4961fa2bb12fe7b426b5aabad96c00a466
Santo Matthew <59092408+santomatthew@users.noreply.github.com>
2021-10-30 20:40:07 +0700

    mengupdate script.js
    menambahkan script untuk menambahkan kelas dan script untuk alert

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-function alert(){
-            alert("Clicked");
+function pencetan(){
+            document.getElementById("tombol").className="btn btn-primary";
+            document.getElementById("tombol").onclick=()=>{
+                alert("Terima kasih telah mengisi feedback");
+            }
         }
